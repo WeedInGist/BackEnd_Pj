1e4f6592b5300730821cf4f276820bced96ed273
Seunghui98 <hithere1012@naver.com>
2022-05-15 17:11:49 +0900

    add google json using kakao service

---
+++ pet/src/main/java/com/_3to1/pet/controller/UserController.java
@@ -0,0 +1,26 @@
+package com._3to1.pet.controller;
+
+import com._3to1.pet.model.service.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+@Controller
+public class UserController {
+    @Autowired
+    private KakaoService kakaoService;
+
+    @RequestMapping("/login")
+    public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
+        System.out.println("#########" + code);
+        String access_Token = kakaoService.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
+        System.out.println("###access_Token#### : " + access_Token);
+        System.out.println("###userInfo#### : " + userInfo.get("email"));
+        System.out.println("###nickname#### : " + userInfo.get("nickname"));
+        System.out.println("###profile_image#### : " + userInfo.get("profile_image"));
+        return "testPage";
+    }
+}
