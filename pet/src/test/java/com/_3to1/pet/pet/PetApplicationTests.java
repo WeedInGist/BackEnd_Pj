1b47c06e05e24d80d4d3493d8429946b4e38f599
LeeSeungHui <hithere1012@naver.com>
2022-05-15 16:13:20 +0900

    Spring boot setting

---
+++ pet/src/test/java/com/_3to1/pet/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package com._3to1.pet.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
