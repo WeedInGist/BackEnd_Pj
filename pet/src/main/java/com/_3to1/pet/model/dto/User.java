6c23ebe9d1a0e5d193e1a5c8c496a15fa6ed0779
조성민 <chob5864@gmail.com>
2022-05-19 09:55:37 +0900

    user dto added, UserRegisterController added only for register

---
+++ pet/src/main/java/com/_3to1/pet/model/dto/User.java
@@ -0,0 +1,17 @@
+package com._3to1.pet.model.dto;
+
+import java.sql.Timestamp;
+
+public class User {
+    private String email;
+    private String password;
+    private String phone;
+    private String gender;
+    private Timestamp birth;
+    private String name;
+    private String nickname;
+    private String region;
+    private int pet;
+    private String role;
+    private int connect;
+}
