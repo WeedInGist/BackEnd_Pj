541b1808de8ef3fc9fe8ffba99fe920e77f50cb7
Seunghui98 <hithere1012@naver.com>
2022-05-19 21:20:16 +0900

    usercontroller

--- pet/src/main/java/com/_3to1/pet/controller/UserController.java
+++ pet/src/main/java/com/_3to1/pet/controller/UserController.java
@@ -1,19 +1,32 @@
 package com._3to1.pet.controller;
 
+import com._3to1.pet.model.UserDto;
+import com._3to1.pet.model.service.JwtTokenProvider;
 import com._3to1.pet.model.service.KakaoService;
+import com._3to1.pet.model.service.UserServie;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
     @Autowired
     private KakaoService kakaoService;
+    private UserServie userService;
 
-    @RequestMapping("/login")
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    @GetMapping("/login")
     public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
         System.out.println("#########" + code);
         String access_Token = kakaoService.getAccessToken(code);
@@ -23,5 +36,16 @@ public class UserController {
         System.out.println("###nickname#### : " + userInfo.get("nickname"));
         System.out.println("###profile_image#### : " + userInfo.get("profile_image"));
         return "testPage";
+    }
+
+    @GetMapping("/defaultlogin")
+    public String login(@RequestBody Map<String, String> user) throws Exception {
+        UserDto member =  userService.userInfo(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        System.out.println("로그인 성공");
+        return jwtTokenProvider.createToken(member.getUsername(), Collections.singletonList("USER"));
     }
 }
